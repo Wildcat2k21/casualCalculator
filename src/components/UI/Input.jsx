8d18cb01516462d7d850b596345b0735fab42c3d
wildcat2k21 <djerse12@mail.ru>
2025-03-23 15:10:40 +0300

    enter handler event

--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -1,17 +1,20 @@
 import { forwardRef } from "react";
 import styles from "./Input.module.css";
 
-const Input = forwardRef(({ value, onChange, onBlur, labelledby }, ref) => (
-  <input
-    ref={ref}
-    onChange={onChange}
-    onBlur={onBlur}
-    className={styles["input-cell"]}
-    aria-labelledby={labelledby}
-    type="text"
-    maxLength={6}
-    value={value}
-  />
-));
+const Input = forwardRef(
+  ({ value, onChange, onKeyDown, onBlur, labelledby }, ref) => (
+    <input
+      ref={ref}
+      onChange={onChange}
+      onBlur={onBlur}
+      onKeyDown={onKeyDown}
+      className={styles["input-cell"]}
+      aria-labelledby={labelledby}
+      type="text"
+      maxLength={6}
+      value={value}
+    />
+  ),
+);
 
 export default Input;
